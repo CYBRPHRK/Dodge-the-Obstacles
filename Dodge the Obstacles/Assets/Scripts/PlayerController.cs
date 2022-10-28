420ce497daad59ada12fa939a5f782e50263e32f
Shivam Singla <46041512+CYBRPHRK@users.noreply.github.com>
2022-10-27 23:10:20 -0300

    Finished the Unit 1 Project for Unity. The truck is now able to move horizontally and vertically as per user inputs. Camera follows the truck as per scripted in FollowPlayer.cs script. An empty object "Obstacles" is created in the scene to keep all the obstacle boxes.

--- Dodge the Obstacles/Assets/Scripts/PlayerController.cs
+++ Dodge the Obstacles/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Getting the player inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Turn the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
